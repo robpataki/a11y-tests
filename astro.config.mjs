8c190562a62a5fa66a1fb9ef070326e1ddc9befb
Rob Pataki <rob.pataki@icloud.com>
2025-02-03 11:17:21 +0000

    Update deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   site: "https://robpataki.github.io",
   base: "a11y-tests",
 });
