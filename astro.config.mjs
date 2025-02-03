d5f04b8598739f8505849e99e4d5704a357a3a91
Rob Pataki <rob.pataki@icloud.com>
2025-02-03 11:08:53 +0000

    Set up GH Pages deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://robpataki.github.io",
+  base: "a11y-tests",
+});
